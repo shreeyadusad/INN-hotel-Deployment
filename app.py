66c7cad4ee33c74571e4963c779bff4036c0745f
shreeyadusad <shreeyadusad@gmail.com>
2024-12-17 12:29:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ with open('transformer.pkl','rb') as file:
 
 def prediction(input_list):
 
-    input_list = np.array(input_list,dtype=object)
     
     pred = model.predict_proba([input_list])[:,1][0]
     
